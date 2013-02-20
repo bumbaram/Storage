82395929a4034b4d56a7868c88fe7b2efff5f272
bumbaram <spam@lagner.ru>
2013-02-21 02:19:16 +0700

    Add base routes.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser('very secret Service'));
   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
@@ -29,8 +29,14 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/file/:id', routes.getFile);
+
+app.post('/file', routes.addFile);
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,41 @@
 /*
  * GET home page.
  */
+var fs 		= require('fs');
+var util 	= require('util');
+
+var MAX_UPLOAD_SIZE = 1048576 * 10; // 10 mb
+var BASE_DIR = __dirname + "/../public/files/";
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+};
+	
+exports.getFile = function(req, res) {
+	var file = req.params.id;
+	console.log("get file :" + file);
+	res.end("File id" + file);
+};
+
+exports.addFile = function(req, res) {
+
+	var file = req.files.file;
+	var tmpFile = file.path;
+	var newFile = BASE_DIR + file.name;
+	
+	console.log("replace " + tmpFile + " to " + newFile);
+
+	var is = fs.createReadStream(tmpFile);
+	var os = fs.createWriteStream(newFile);
+
+	util.pump(is, os, function() {
+		fs.unlink(tmpFile, function(err) {
+			if (err) {
+				res.end("Error");
+				console.log("FATAL!!!");
+			} else {
+				res.redirect("/");
+			}
+		})
+	})
 };
